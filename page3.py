64bc33b88907f5971d9272ab490edd1e01965f70
sayali144 <gsayali144@gmail.com>
2017-03-25 11:16:56 +0530

    Add files via upload

---
+++ page3.py
@@ -0,0 +1,43 @@
+import sys
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+class Form(QDialog):
+   def __init__(self, parent=None):
+      super(Form, self).__init__(parent)
+		
+      layout = QVBoxLayout()
+      self.b1 = QPushButton("Execute or Submit")
+      self.b1.setCheckable(True)
+      self.b1.toggle()
+      self.b1.clicked.connect(lambda:self.whichbtn(self.b1))
+      self.b1.clicked.connect(self.btnstate)
+      layout.addWidget(self.b1)
+		
+     
+      self.setLayout(layout)
+     # self.b3 = QPushButton("Submit")
+     # self.b3.setEnabled(True)
+      #layout.addWidget(self.b3)
+		
+      
+      
+      self.setWindowTitle("Page 3")
+
+   def btnstate(self):
+      if self.b1.isChecked():
+         print "button pressed"
+      else:
+         print "button released"
+			
+   def whichbtn(self,b):
+      print "clicked button is "+b.text()
+
+def main():
+   app = QApplication(sys.argv)
+   ex = Form()
+   ex.show()
+   sys.exit(app.exec_())
+	
+if __name__ == '__main__':
+   main()
