53108b4114bf9dbed3a32c502164323f45acb0ab
sayali144 <gsayali144@gmail.com>
2017-03-25 12:02:13 +0530

    Add files via upload

---
+++ page1.py
@@ -0,0 +1,57 @@
+from PyQt4 import QtGui, QtCore
+import os, sys
+
+
+class PrettyWidget(QtGui.QWidget):
+    
+    def __init__(self):
+        super(PrettyWidget, self).__init__()
+        self.initUI()
+        
+        
+    def initUI(self):
+        self.setGeometry(600, 500, 500, 500)
+        self.setWindowTitle('Multiple Browse') 
+	    
+        
+        btn = QtGui.QPushButton('Browse\n(MULTIPLE)', self)
+	
+	#btn.setGeometry(200, 200, 350, 120) 
+	btn.resize(btn.sizeHint())
+        btn.clicked.connect(self.SingleBrowse)
+	btn.resize(150,150)
+        btn.move(150, 100) 
+        go_button = QtGui.QPushButton("next", self)
+        #go_button.resize(250,250)
+        go_button.move(180, 280)
+	go_button.setDefault(True)
+	   
+
+        self.show()
+
+  
+
+
+    def SingleBrowse(self):
+        filePaths = QtGui.QFileDialog.getOpenFileNames(self, 
+                                                       'Multiple File',
+                                                       "~/Desktop/PyRevolution/PyQt4",
+                                                      '*.xlsx')
+        for filePath in filePaths:
+            print('filePath',filePath, '\n')
+            fileHandle = open(filePath, 'r')
+            lines = fileHandle.readlines()
+            for line in lines:
+                print(line)
+	
+
+    
+def main():
+    app = QtGui.QApplication(sys.argv)
+    w = PrettyWidget()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
+s
